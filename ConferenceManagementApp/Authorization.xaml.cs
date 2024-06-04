a43b8d4bbba916c99b4cf581dbb640e9c4431319
dread <dread@REFLEX>
2024-06-05 04:47:34 +0900

    Добавьте файлы проекта.

---
+++ ConferenceManagementApp/Authorization.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ConferenceManagementApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        //private string connectionString = "Data Source=REFLEXLAPTOP;Initial Catalog=PR1_1;Integrated Security=True"; //ноут
+        private string connectionString = "Data Source=ReFlex;Initial Catalog=PR1_1;Integrated Security=True"; //пк
+
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Password;
+
+            // Выполните запрос к базе данных, чтобы получить роль пользователя
+            string role = GetRoleFromDatabase(username, password);
+
+            if (role != null)
+            {
+                OpenMainWindow(role);
+            }
+            else
+            {
+                MessageBox.Show("Неверное имя пользователя или пароль");
+            }
+        }
+
+        private string GetRoleFromDatabase(string username, string password)
+        {
+            string role = null;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT role FROM Users WHERE username = @username AND password = @password";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", password);
+
+                try
+                {
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+                    if (result != null)
+                    {
+                        role = result.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при выполнении запроса к базе данных: " + ex.Message);
+                }
+            }
+
+            return role;
+        }
+
+        private void OpenMainWindow(string role)
+        {
+            Window mainWindow = null;
+
+            switch (role)
+            {
+                case "1":
+                    mainWindow = new AdminWindow();
+                    break;
+                case "2":
+                    mainWindow = new ResearcherWindow();
+                    break;
+                case "3":
+                    mainWindow = new GuestWindow();
+                    break;
+                case "4":
+                    mainWindow = new MainWindow();
+                    break;
+                default:
+                    MessageBox.Show("Неизвестная роль пользователя.");
+                    return;
+            }
+
+            if (mainWindow != null)
+            {
+                mainWindow.Show();
+                this.Close();
+            }
+        }
+
+        private void GuestLoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            GuestWindow guest = new GuestWindow();
+            this.Close();
+            guest.Show();
+        }
+    }
+}
---
+++ ConferenceManagementApp/GuestWindow.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ConferenceManagementApp
+{
+    /// <summary>
+    /// Логика взаимодействия для GuestWindow.xaml
+    /// </summary>
+    public partial class GuestWindow : Window
+    {
+        //private string connectionString = "Data Source=REFLEXLAPTOP;Initial Catalog=PR1_1;Integrated Security=True"; //ноут
+        private string connectionString = "Data Source=ReFlex;Initial Catalog=PR1_1;Integrated Security=True"; //пк
+
+        public List<Researcher> Researchers { get; set; }
+        public List<Conference> Conferences { get; set; }
+        public List<AnalysisResult> AnalysisResults { get; set; }
+
+        public GuestWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        public class AnalysisResult
+        {
+            public string FullName { get; set; }
+            public int NumberOfPresentations { get; set; }
+        }
+
+        private void LoadData()
+        {
+            Researchers = new List<Researcher>();
+            Conferences = new List<Conference>();
+            LoadResearchers();
+            LoadConferences();
+            analysisResultsDataGrid.ItemsSource = AnalysisResults;
+
+            AnalysisResults = new List<AnalysisResult>();
+            string query = "SELECT full_name, COUNT(*) AS number_of_presentations " +
+                           "FROM Researchers " +
+                           "INNER JOIN Participation ON Researchers.id = Participation.researcher_id " +
+                           "GROUP BY full_name";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        AnalysisResults.Add(new AnalysisResult
+                        {
+                            FullName = reader.GetString(0),
+                            NumberOfPresentations = reader.GetInt32(1)
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading analysis data: " + ex.Message);
+                }
+            }
+
+            analysisResultsDataGrid.ItemsSource = AnalysisResults;
+        }
+
+        private void LoadResearchers()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Researchers";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Researchers.Add(new Researcher
+                        {
+                            Id = reader.GetInt32(0),
+                            FullName = reader.GetString(1),
+                            Country = reader.GetString(2),
+                            AcademicDegree = reader.GetString(3)
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading researchers: " + ex.Message);
+                }
+            }
+        }
+
+        private void LoadConferences()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Conferences";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Conferences.Add(new Conference
+                        {
+                            ConferenceCode = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Date = reader.GetDateTime(2),
+                            Location = reader.GetString(3)
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading conferences: " + ex.Message);
+                }
+            }
+        }
+            
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            searchErrorLabel.Content = "";
+            if (string.IsNullOrEmpty(fullNameSearchTextBox.Text))
+            {
+                searchErrorLabel.Content = "Вы не заполнили все поля";
+                return;
+            }
+            string searchQuery = "SELECT Conferences.name, Conferences.date, Conferences.location, Participation.topic " +
+                                 "FROM Researchers " +
+                                 "INNER JOIN Participation ON Researchers.id = Participation.researcher_id " +
+                                 "INNER JOIN Conferences ON Participation.conference_code = Conferences.conference_code " +
+                                 "WHERE Researchers.full_name = @full_name " +
+                                 "ORDER BY Conferences.date DESC";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(searchQuery, connection);
+                command.Parameters.AddWithValue("@full_name", fullNameSearchTextBox.Text);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    searchResultsDataGrid.ItemsSource = dataTable.DefaultView;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+        private void ChangeAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            Authorization auth = new Authorization();
+            this.Close();
+            auth.Show();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            CustomMessageBox customMessageBox = new CustomMessageBox();
+            customMessageBox.ShowDialog();
+        }
+    }
+}
